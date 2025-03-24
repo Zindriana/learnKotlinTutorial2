e555ae77b0cae935b11cc4b84eae26d24739b964
Zindriana <yhmu23.hultjo@folkuniversitetet.nu>
2025-03-24 15:43:14 +0100

    added a composable to test "in" in the when statement

--- app/src/main/java/com/example/tutorial2_android/MainActivity.kt
+++ app/src/main/java/com/example/tutorial2_android/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import com.example.tutorial2_android.ui.theme.Tutorial2_androidTheme
 
@@ -15,8 +21,19 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Tutorial2_androidTheme {
-                val color = "Green"
-                trafficLight(color)
+
+                val color = "green"
+                val number = 11
+                Column(
+                    modifier = Modifier.padding(
+                        WindowInsets.systemBars
+                            .asPaddingValues()
+                    )
+                ) {
+                    trafficLight(color)
+                    isNumberHigh(number)
+                }
+
             }
         }
     }
@@ -25,9 +42,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun trafficLight(color: String) {
     when(color) {
-        "Red" -> Signal("Stop", Color.Red)
-        "Yellow" -> Signal("Slow", Color.Yellow)
-        "Green" -> Signal("Go", Color.Green)
+        "Red", "red", "pink" -> Signal("Stop", Color.Red)
+        "Yellow", "yellow", "Orange" -> Signal("Slow", Color.Yellow)
+        "Green", "torques", "cyan","blue", "green" -> Signal("Go", Color.Green)
         else -> Signal("Invalid traffic-light color", Color.Gray)
     }
 }
@@ -38,6 +55,15 @@ fun Signal(message: String, color: Color) {
         text = message,
         color = color
     )
+}
+
+@Composable
+fun isNumberHigh(number: Int){
+    when {
+        number in 1..9 -> Signal("False", Color.Red)
+        number > 9 -> Signal("True", Color.Green)
+        else -> Signal("Neither", Color.Black)
+    }
 }
 
 
